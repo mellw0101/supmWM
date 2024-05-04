95b65bf63f937941c99a3369c58a976d88c8d19b
mellw0101 <melwivensson@gmail.com>
2024-05-04 22:03:14 +0000

    Changes to be committed: 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "tools/defines.h"
 
 #include <xcb/xcb.h>
+#include <xcb/xproto.h>
 
 #include "window.cpp"
 #include "tools/logger.h"
@@ -13,26 +14,69 @@ xcb_screen_t* screen = nullptr;
 xcb_screen_iterator_t iter;
 const xcb_setup_t* setup = nullptr;
 
-static void quit()
+static void event_handler(xcb_generic_event_t* ev)
 {
-    xcb_flush(conn);
+    uint8_t evType = ev->response_type & ~0x80;
+    switch (evType)
+    {
+        case XCB_EXPOSE:
+        {
 
-    xcb_generic_event_t* ev;
-    
-    while (xcb_poll_for_event(conn));
+        }
+    }
 }
-
-int main()
+xcb_void_cookie_t cookie = xcb_change_window_attributes_checked
+                (
+                    conn,
+                    root,
+                    XCB_CW_EVENT_MASK,
+                    (const uint32_t[1])
+                    {
+                        XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY
+                    }
+                );
+                xcb_generic_error_t *error = xcb_request_check( conn, cookie );
+                if ( !error ) return true;
+                
+                loutE << "Error: Another window manager is already running or failed to set SubstructureRedirect mask" << loutEND;
+                free( error );
+static void setup_wm()
 {
     conn = xcb_connect(nullptr, nullptr);
-    if (xcb_connection_has_error(conn) == 0)
-    {
-        cout << "SuccessBALLA\n";
-    }
-
     setup = xcb_get_setup(conn);
     iter = xcb_setup_roots_iterator(setup);
     screen = iter.data;
 
-    loutE << "screen->width_in_pixels" << screen->width_in_pixels << " screen->height_in_pixels" << screen->height_in_pixels << '\n';
+    xcb_void_cookie_t cookie = xcb_change_window_attributes_checked
+    (
+        conn,
+        screen->root,
+        XCB_CW_EVENT_MASK,
+        (const uint32_t[1])
+        {
+            XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY
+        }
+    );
+
+    xcb_generic_error_t *error = xcb_request_check(conn, cookie);
+    if (error)
+    {
+        loutE << "Error: Another window manager is already running or failed to set SubstructureRedirect mask" << loutEND;
+        free(error);
+    }
+}
+
+int main()
+{
+    setup_wm();
+
+    xcb_generic_event_t* ev = nullptr;
+
+    while (true)
+    {
+        ev = xcb_wait_for_event(conn);
+        if (!ev) continue;
+
+        event_handler(ev);
+    }
 }
